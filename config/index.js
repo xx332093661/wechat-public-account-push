2d906c92a535fd816ff569e563be5643bcccf481
xx332093661 <332093661@qq.com>
2022-09-03 12:20:47 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -33,7 +33,7 @@ export const config = {
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "3iPJAEpeA-DCAGSHqirTo0QOw7UGGKdSqOdDvCqLmpk",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn"
+      openUrl: "http://loveyue1.bigeone.cn/"
     }, 
   ],
 
